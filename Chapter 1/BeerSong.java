c6c3643a9fb8ba6c7d25d685c1e11c6cb0a07911
[M4yankChoudhary] <[mayankchd10@gmail.com]>
2019-01-19 18:41:11 +0530

    chapter 1

---
+++ Chapter 1/BeerSong.java
@@ -0,0 +1,31 @@
+public class BeerSong {
+	public static void main (String[] args) {
+		int beerNum = 99;
+		String word = "bottles";
+		
+		while (beerNum > 0) {
+			System.out.println(beerNum + " " + word + " of beer on the wall");
+		        System.out.println(beerNum + " " + word + " of beer.");
+                        System.out.println("Take one down.");
+                        System.out.println("pass it around.");
+			beerNum = beerNum - 1;
+		
+
+                        if (beerNum == 1){
+				word = "bottle";
+		
+			}
+
+			if (beerNum > 0) {
+				System.out.println(beerNum + " " + word + " of beer on the wall");
+			}
+			else
+			{
+	
+				System.out.println("No more bottles of beer on the wall");
+			}
+		}
+	}
+}
+
+
